93d0b715e5a4b15f4035d3bfbf653f9a417e3b2c
Stephen Hong <mrsakkaro@dream-farmer.com>
2017-06-13 09:11:35 +0900

    Overhauled so it would actually work

--- watchhddtemp.sh
+++ watchhddtemp.sh
@@ -1,33 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
 ALERT_LEVEL=50
 SEND_ALERT=0
 
 MESSAGE="Hard Drive Temperature Report at `date`: "
-MESSAGE=$MESSAGE'\n\n'
+MESSAGE+=$'\n\n'
 
 SERVER="aspmx.l.google.com"
-FROM="FROM EMAIL"
-TO="TO EMAIL"
+FROM="From address"
+TO="Email Address to Send"
 CHARSET="utf-8"
 SUBJ="Hard Drive Temperature Report at `date`"
+
+TEMP=""
 
 
 for DEV in /dev/sd*
 do
-	if [ $(hddtemp $DEV | awk '{print $NF}' | awk -F '°' '{ print $1}') -ge $ALERT_LEVEL ]; then
+	STR_CONCAT=`/usr/sbin/hddtemp $DEV`
+	#TEMP= $(hddtemp $DEV | awk '{print $NF}' | awk -F '°' '{ print $1}')
+	if [[ $(/usr/sbin/hddtemp $DEV | awk '{print $NF}' | awk -F '°' '{ print $1}') -ge $ALERT_LEVEL ]]; then
 		SEND_ALERT=1
 	fi
-	STR_CONCAT=`hddtemp $DEV`
-	MESSAGE=$MESSAGE$STR_CONCAT'\n'
-	#hddtemp $DEV >> temp.txt
+	MESSAGE+=$STR_CONCAT
+	MESSAGE+=$'\n'
 done
 
-echo $MESSAGE
+#echo "$MESSAGE" > temp.txt
 
 if [ $SEND_ALERT -ge 1 ]; then
 	SUBJ="ALERT: HARD DRIVE TEMPERATURE REPORT AT `date`"
-	sendemail -f $FROM -t $TO -s $SERVER -v -o message-charset=$CHATSET -u $SUBJ -m $MESSAGE
+	sendemail -f $FROM -t $TO -s $SERVER -v -o message-charset=$CHATSET -u $SUBJ -m "$MESSAGE"
 else
-	sendemail -f $FROM -t $TO -s $SERVER -v -o message-charset=$CHATSET -u $SUBJ -m $MESSAGE
+	sendemail -f $FROM -t $TO -s $SERVER -v -o message-charset=$CHATSET -u $SUBJ -m "$MESSAGE"
 fi
